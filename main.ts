4d426e0a3f16d57e6b73863a886d5414e4913afe
Nico <nickypellerin@gmail.com>
2020-06-15 18:30:40 -0400

    Adds auto-updater

--- main.ts
+++ main.ts
@@ -8,7 +8,11 @@ import {
   MenuItemConstructorOptions,
   nativeImage,
   shell,
+  autoUpdater,
+  dialog,
 } from "electron"
+
+// require("update-electron-app")()
 
 import MainWindow from "./MainWindow"
 
@@ -185,3 +189,34 @@ ipcMain.on("updateTrayIcon", () => {
 ipcMain.on("updateTrayIconNotifsSeen", () => {
   tray.setImage(trayIcon)
 })
+
+// Auto updater
+// const server =
+//   "https://github.com/nicopellerin/favlinkz-desktop/releases/latest"
+// const url = `${server}/update/${process.platform}/${app.getVersion()}`
+
+// autoUpdater.setFeedURL({ url })
+
+// setInterval(() => {
+//   autoUpdater.checkForUpdates()
+// }, 60000)
+
+// autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
+//   const dialogOpts = {
+//     type: "info",
+//     buttons: ["Restart", "Later"],
+//     title: "Application Update",
+//     message: process.platform === "win32" ? releaseNotes : releaseName,
+//     detail:
+//       "A new version has been downloaded. Restart the application to apply the updates.",
+//   }
+
+//   dialog.showMessageBox(dialogOpts).then((returnValue) => {
+//     if (returnValue.response === 0) autoUpdater.quitAndInstall()
+//   })
+// })
+
+// autoUpdater.on("error", (message) => {
+//   console.error("There was a problem updating the application")
+//   console.error(message)
+// })
